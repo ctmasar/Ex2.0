073c153ec7645fa11741c799e08f8bdec1fd9d7f
ctmasar <77703403+ctmasar@users.noreply.github.com>
2021-01-24 19:08:03 -0500

    Execise 2.0
    My Exercise 2.0 assignment

---
+++ Ex2.0.py
@@ -0,0 +1,10 @@
+name = 'Chance Masar'
+date = '1/19/2021'
+question = 'At what size of a project should you start using version management software? Why?'
+response = 'I would use version management software for any large project really, and/or when it is not just me working on the project. Having the ability to revert to a previous version of the program is helpful when bugs arise, also for the fact that there is sort of a backup of the program somewhere in the case something goes wrong. The branching capabilities are fairly cool to have as well. It certainly is useful to have when you always have a live version of the program, but you also want to be able to update the program as new things get added. Additionally, as we had learned in the video, the branching feature makes it useful when more than one person is working on the project at hand. It undoubtedly is a powerful, if not necessary, tool to have for any major project that an individual may be working on.'
+
+ary = [name, date, question, response]
+for i in range(4):
+    print(ary[i])
+    print('')
+    pass
